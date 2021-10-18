6973cbcc7777b11df1fa97b3ff2dc3c79774cd55
Bana Khusnan <banakhsn04@gmail.com>
2021-10-18 12:58:56 +0700

    menambahkan script jquery dan ajax

--- index.php
+++ index.php
@@ -34,33 +34,38 @@ if (isset($_POST["search"])){
     <br>
     <a href="addmahasiswa.php">Add Student</a>
 
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>NPM</th>
-            <th>Email</th>
-            <th>Foto</th>
-            <th>Action</th>
-        </tr>
+    <div id="container">
+        <table border="1" cellpadding="10" cellspacing="0">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NPM</th>
+                <th>Email</th>
+                <th>Foto</th>
+                <th>Action</th>
+            </tr>
 
-        <?php $i = 1 ?>
-        <?php foreach($mahasiswa as $row) : ?>
-        <tr>
-            <td><?= $i; ?></td>
-            <td><?php echo $row["nama"]; ?></td>
-            <td><?php echo $row["npm"]; ?></td>
-            <td><?php echo $row["email"]; ?></td>
-            <td>
-                <img src="img/<?php echo $row["gambar"]; ?>" width="50">
-            </td>
-            <td>
-                <a href="editMahasiswa.php?id=<?php echo $row["idmahasiswa"]; ?>">Edit</a> |
-                <a href="delete.php?id=<?php echo $row["idmahasiswa"]; ?>">Delete</a>
-            </td>
-        </tr>
-        <?php $i++; ?>
-        <?php endforeach; ?>
-    </table>
+            <?php $i = 1 ?>
+            <?php foreach($mahasiswa as $row) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td><?php echo $row["nama"]; ?></td>
+                <td><?php echo $row["npm"]; ?></td>
+                <td><?php echo $row["email"]; ?></td>
+                <td>
+                    <img src="img/<?php echo $row["gambar"]; ?>" width="50">
+                </td>
+                <td>
+                    <a href="editMahasiswa.php?id=<?php echo $row["idmahasiswa"]; ?>">Edit</a> |
+                    <a href="delete.php?id=<?php echo $row["idmahasiswa"]; ?>">Delete</a>
+                </td>
+            </tr>
+            <?php $i++; ?>
+            <?php endforeach; ?>
+        </table>
+    </div>
+
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/script.js"></script>
 </body>
 </html>
